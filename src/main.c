ebb5f0b8c0208330bbb587a775414a5239813229
0x3C-0x3E <rbng0094@gmail.com>
2025-06-27 22:39:31 +0200

    added ir step, still in the works, heap corruption as ususal

--- src/main.c
+++ src/main.c
@@ -67,6 +67,57 @@ int parse_chars() {
     return 0; 
 }
 
+typedef enum {
+    IR_DEC_DP = '>',
+    IR_INC_DP = '<',
+    IR_ADD = '+',
+    IR_SUB = '-',
+    IR_OUT = '.',
+    IR_IN = ',',
+    IR_JIZ = '[',
+    IR_JNZ = ']',
+} IR_Type;
+
+typedef struct {
+    IR_Type type;
+
+    // this either describes the amount gathered by the IR operation of how often to execute the type
+    // or the index of where to jump to if IR_JIZ or IR_JNZ
+    int operation;
+} IROperation;
+
+DArray ir_operations = {0};
+
+
+void handle_ir_operation(DArray* ir_operations, IROperation* prev_operation, IR_Type type) {
+    if (prev_operation->type == type) {
+        log_msg(LOG_INFO, "adding +1 to IRTYPE: %d\n", type);
+        prev_operation->operation += 1;
+        return;
+    }
+    
+    *prev_operation = (IROperation) {
+        .type = type,
+        .operation = 1,
+    };
+
+    log_msg(LOG_INFO, "found new IRTYPE: %c\n", type);
+
+    d_array_append(ir_operations, prev_operation);
+}
+
+int generate_ir() {
+    ir_operations = d_array_create(sizeof(IROperation));
+    IROperation prev_operation;
+
+    for (size_t i = 0; i < parsed_chars.size; i++) {
+        char c = *(char*)d_array_get(&parsed_chars, i);
+        IR_Type t = (IR_Type) c;
+        handle_ir_operation(&ir_operations, &prev_operation, t);
+    } 
+    return 0; 
+}
+
 int main(int argc, char** argv) {
     if (argc < 2) {
         log_msg(LOG_CRITICAL_ERROR, "insufficiant number of arguments provided}\n");
@@ -78,6 +129,10 @@ int main(int argc, char** argv) {
     }
 
     if (parse_chars() != 0) {
+        return 1;
+    }
+
+    if (generate_ir() != 0) {
         return 1;
     }
 
