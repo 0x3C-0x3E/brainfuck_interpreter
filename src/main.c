627766d03da6b984606c51a8e2014dba4e2d356e
0x3C-0x3E <rbng0094@gmail.com>
2025-07-01 19:13:10 +0200

    made into single file

--- src/main.c
+++ src/main.c
@@ -1,6 +1,3 @@
-#include "d_array.h"
-#include "log_util.h"
-#include "file_handler.h"
 #include <stdio.h>
 #include <stdarg.h>
 #include <stdlib.h>
@@ -9,6 +6,58 @@
 
 #define VERBOSE_OUTPUT 0
 #define DEBUG 0
+
+typedef enum {
+    LOG_INFO,
+    LOG_ERROR,
+    LOG_CRITICAL_ERROR,
+    LOG_DUMP,
+    LOG_INTERP_DEBUG,
+} LOG_TYPE;
+
+// stuff that lets gcc check if i am wrong, which i am most of the time
+__attribute__((format(printf, 2, 3)))
+void log_msg(LOG_TYPE type, const char* format, ...);
+
+typedef struct {
+    // ptr to start of data
+    void* data;
+    
+    // theoretical capacity
+    size_t capacity;
+
+    // actual amout of elements 
+    size_t size;
+
+    // individual size of the element
+    size_t data_size;
+} D_Array;
+
+// this returns a pointer to a heap-allocated array with size 0, so no 
+// elements are inserted at creation and the array is empty
+D_Array* d_array_new(size_t data_size);
+
+// void* data gets copied into the array so there is no 
+// need to check for livetimes
+void d_array_append(D_Array* d_array, void* data);
+
+void* d_array_pop(D_Array* d_array);
+
+void* d_array_get(D_Array* d_array, int index);
+
+void d_array_remove(D_Array* d_array, int index);
+
+void d_array_free(D_Array* d_array);
+
+
+typedef struct InputFile {
+    const char* filepath;
+    FILE* file_ptr;
+    size_t file_size;
+
+} InputFile;
+
+int init_file(const char* filepath, InputFile* input_file);
 
 InputFile input_file = {0};
 D_Array* parsed_chars;
@@ -45,6 +94,124 @@ typedef struct {
 } BF_Interpreter;
 
 BF_Interpreter bf_interpreter = {0};
+
+__attribute__((format(printf, 2, 3)))
+void log_msg(LOG_TYPE type, const char* format, ...) {
+    char* log_msg;
+    switch (type) {
+        // case LOG_INFO: log_msg = "INFO"; break;
+        case LOG_ERROR: log_msg = "ERROR"; break;
+        case LOG_DUMP: log_msg = "D"; break;
+        case LOG_CRITICAL_ERROR: log_msg = "CRIT_ERROR"; break;
+        // case LOG_INTERP_DEBUG: log_msg = "DEBUG"; break;
+        // case LOG_DUMP: log_msg = "D"; return;
+        case LOG_INTERP_DEBUG: log_msg = "DEBUG"; return;
+        case LOG_INFO: log_msg = "INFO"; return;
+    }
+    printf("[%s] ", log_msg);
+
+    va_list args;
+    va_start(args, format);
+    vprintf(format, args);
+    va_end(args);
+
+}
+
+int init_file(const char* filepath, InputFile* input_file) {
+    input_file->filepath = filepath;
+    log_msg(LOG_INFO, "filepath is: %s\n", input_file->filepath);
+
+    input_file->file_ptr = fopen(input_file->filepath, "r");
+    if (input_file->file_ptr == NULL) {
+        log_msg(LOG_CRITICAL_ERROR, "Error opening file");
+        return 1;
+    }
+
+    fseek(input_file->file_ptr, 0, SEEK_END);
+    input_file->file_size = ftell(input_file->file_ptr);
+    fseek(input_file->file_ptr, 0, SEEK_SET);
+    log_msg(LOG_INFO, "filesize is: %ld\n", input_file->file_size);
+
+    return 0;
+}
+
+D_Array* d_array_new(size_t data_size) {
+    D_Array* new_array = malloc(sizeof(D_Array));
+    if (new_array == NULL) {
+        goto failed_init;
+    }
+
+    void* new_data = malloc(0);
+    if (new_array == NULL) {
+        goto failed_init;
+    }
+
+    *new_array = (D_Array){
+        .data = new_data,
+        .data_size = data_size,
+        .size = 0,
+        .capacity = 0,
+    };
+    return new_array;
+
+failed_init:
+    perror("[ERROR] D_Array failed to init\n");
+    return NULL;
+}
+
+void d_array_append(D_Array* d_array, void* data) {
+    // check if we already exhausted the capacity and need to realloc
+    if (d_array->size == d_array->capacity) {
+        // size_t new_capacity = (d_array->capacity != 0) ? d_array->capacity * 2 : 2;
+
+        size_t new_capacity = d_array->capacity + 1;
+
+        void* new_data = realloc(d_array->data, new_capacity * d_array->data_size);
+        if (new_data == NULL) {
+            perror("[ERROR] D_Array failed to realloc\n");
+            return;
+        }
+        
+        
+        d_array->capacity = new_capacity;
+        d_array->data = new_data;
+    }
+
+    
+    void* target = d_array->data + ((d_array->size) * d_array->data_size);
+    mempcpy(target, data, d_array->data_size);
+
+    d_array->size ++;
+}
+
+void* d_array_pop(D_Array* d_array) {
+    void* target = d_array->data + ((d_array->size - 1) * d_array->data_size);
+    d_array->size --;
+
+    return target;
+}
+
+void* d_array_get(D_Array* d_array, int index) {
+    void* target = d_array->data + (index * d_array->data_size);
+
+    return target;
+}
+
+void d_array_remove(D_Array* d_array, int index) {
+    for (size_t i = index; i < d_array->size; i++) {
+        void* new_value = d_array_get(d_array, i + 1);
+        void* old_value = d_array_get(d_array, i);
+        
+        mempcpy(old_value, new_value, d_array->data_size);
+    }
+
+    d_array->size --;
+}
+
+void d_array_free(D_Array* d_array) {
+    free(d_array->data);
+    free(d_array);
+}
 
 int init_arrays() {
     parsed_chars = d_array_new(sizeof(char));
